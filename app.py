43083387e895a8de309a46e1f877f988f9e74fb1
jpaul21 <jhwang@stevens.edu>
2021-12-15 15:02:43 -0500

    add edit and delete function

--- app.py
+++ app.py
@@ -59,8 +59,22 @@ def edit(id):
 			flash('Title is required!')
 		else:
 			conn = get_db_connection()
-			conn.execute('UPDATE posts SET title = ?, content = ?, WHERE id = ?', (title, content, id))
+			conn.execute('UPDATE posts SET title = ?, content = ?'
+				     ' WHERE id = ?', 
+				     (title, content, id))
 			conn.commit()
 			conn.close()
 			return redirect(url_for('index'))
 	return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+	post = get_post(id)
+	conn = get_db_connection()
+	conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+	conn.commit()
+	conn.close()
+	flash('"{}" was successfully deleted!'.format(post['title']))
+	return redirect(url_for('index'))
+ 
+
